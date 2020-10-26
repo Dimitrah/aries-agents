d6d97f747b149b3edb2acd2f6c4aa64a9f626f1c
DimitraChatziara <37339678+Dimitrah@users.noreply.github.com>
2020-10-26 14:49:42 +0200

    Update faber.py
    schema

--- demo/runners/faber.py
+++ demo/runners/faber.py
@@ -159,7 +159,7 @@ async def main(start_port: int, no_auto: bool = False, show_timing: bool = False
                 _,  # schema id
                 credential_definition_id,
             ) = await agent.register_schema_and_creddef(
-                "identity schema", version, ["name", "date", "amka", "idcard_test"]
+                "identity schema", version, ["full_name", "id_card", "birth_date", "age", "address"]
             )
 
         # TODO add an additional credential for Student ID
